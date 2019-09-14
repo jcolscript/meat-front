a3ea5b903f6971236da8d110bca7175959cc5a42
José Carlos <jcolscript@gmail.com>
2019-09-14 02:14:35 -0300

    ajuste modulo e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { RestaurantsComponent } from './components/restaurants/restaurants.component';
 import { RestaurantComponent } from './components/restaurants/restaurant/restaurant.component'
+import { RestaurantDetailComponent } from './components/restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './components/restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './components/restaurant-detail/menu/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './components/restaurant-detail/menu/menu-item/menu-item.component';
+import { ReviewsComponent } from './components/restaurant-detail/reviews/reviews.component';
 
 import { RestaurantsService } from './services/restaurants.service';
 
@@ -21,7 +26,12 @@ import { RestaurantsService } from './services/restaurants.service';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { AboutComponent } from "./components/about/about.component";
 import { RestaurantsComponent } from "./components/restaurants/restaurants.component";
+import { RestaurantDetailComponent } from "./components/restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./components/restaurant-detail/menu/menu.component";
+import { ReviewsComponent } from "./components/restaurant-detail/reviews/reviews.component";
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurants', component: RestaurantsComponent },
+  { path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      { path: '', redirectTo: 'menu', pathMatch: 'full'},
+      { path: 'menu', component: MenuComponent },
+      { path: 'reviews', component: ReviewsComponent }
+    ]
+  },
   { path: 'about', component: AboutComponent }
 ]
