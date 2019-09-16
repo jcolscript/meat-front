6c05bf6c1fc47db3431af4c55fd139b70c5369b4
José Carlos <j.de.oliveira.lima@avanade.com>
2019-09-15 21:39:38 -0300

    pequenos ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -42,7 +42,8 @@ import { ShoppingCartService } from './services/shopping-cart.service';
   ],
   providers: [
     RestaurantsService,
-    ShoppingCartService
+    ShoppingCartService,
+    {provide: LOCALE_ID, useValue: 'pt-br'}
   ],
   bootstrap: [AppComponent]
 })
