68da76dcfd85937be1becd3de447d8cd36321b7e
José Carlos <jcolscript@gmail.com>
2019-10-13 20:33:53 -0300

    animacoes no componente de review

--- src/app/components/restaurant-detail/reviews/reviews.component.ts
+++ src/app/components/restaurant-detail/reviews/reviews.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { RestaurantsService } from 'app/services/restaurants.service';
 import { ActivatedRoute } from '@angular/router';
@@ -7,11 +8,21 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.css']
+  styleUrls: ['./reviews.component.css'],
+  animations: [
+    trigger('reviewsAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(0px, 50px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class ReviewsComponent implements OnInit {
 
   public reviews: Observable<any>;
+  public reviewState = 'ready';
 
   constructor(
     private restaurantsService: RestaurantsService,
