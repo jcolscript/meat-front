406f37ce15f52ce2ee2690a451a1654ca25a1315
José Carlos <jcolscript@gmail.com>
2019-10-13 20:20:59 -0300

    adicionando animacao na lista de restaurates

--- src/app/components/restaurants/restaurant/restaurant.component.ts
+++ src/app/components/restaurants/restaurant/restaurant.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { Restaurant } from 'app/models/restaurant.model';
 
 @Component({
   selector: 'mt-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.css']
+  styleUrls: ['./restaurant.component.css'],
+  animations: [
+    trigger('restaurantsAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(0px, 50px)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
 
   @Input() restaurant: Restaurant;
+  public restaurantState = 'ready';
 
   constructor() { }
 
