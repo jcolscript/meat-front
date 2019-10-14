681bee76f9015a7a286a266fff50ac468a6f6f91
José Carlos <j.de.oliveira.lima@avanade.com>
2019-10-14 18:54:24 -0300

    criando a barra de pesquisa para restaurantes

--- src/app/components/restaurants/restaurants.component.ts
+++ src/app/components/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 import { Restaurant } from 'app/models/restaurant.model';
 
@@ -7,17 +8,36 @@ import { RestaurantsService } from 'app/services/restaurants.service';
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
   public restaurants: Restaurant[];
+  public searchBarState = 'hidden';
 
   constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
     this.restaurantsService.Restaurants
       .subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+  toggleSearchBar() {
+    this.searchBarState = this.searchBarState == 'hidden' ? 'visible' : 'hidden';
   }
 
 }
